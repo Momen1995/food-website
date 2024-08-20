5dc8f4caa469f92772d34698e74c907801b21603
Momen1995 <momensrk@gmail.com>
2024-08-20 11:16:04 +0600

    modal add

--- main.js
+++ main.js
@@ -3,12 +3,16 @@
 const header = document.querySelector("header");
 const nav = document.querySelector(".navs");
 const allSections = document.querySelector(".section");
+const modal = document.querySelector(".modal");
+const allBtn = document.querySelectorAll(".btn");
+const closBtn = document.querySelector(".btn-close-modal");
+const overlay = document.querySelector(".overlay");
 
 //nav height find
 const navHeight = nav.getBoundingClientRect().height;
 
 //stick navbar
-function sticky(entries) {
+function sticky(entries, observe) {
   const entry = entries[0];
 
   if (!entry.isIntersecting) nav.classList.add("sticky");
@@ -24,17 +28,29 @@ const headerObserver = new IntersectionObserver(sticky, {
 headerObserver.observe(header);
 
 //section reveal
-function revealSection(entries){
+function revealSection(entries) {
   const [entry] = entries;
-  console.log(entry)
-
-  if(entry.isIntersecting) entry.target.classList.remove("section-hidden");
+  if (entry.isIntersecting) entry.target.classList.remove("section-hidden");
 }
-const sectionObserver = new IntersectionObserver(revealSection,{
+
+const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold : 0.2,
-})
+  threshold: 0.2,
+});
 
- sectionObserver.observe(allSections)
+sectionObserver.observe(allSections);
 
+//modal
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
 
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+allBtn.forEach((btn) => btn.addEventListener("click", openModal));
+closBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
